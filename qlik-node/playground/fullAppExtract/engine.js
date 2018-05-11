93c9718ae9c028567efd1ab1351641ada72b2cc4
Cristi Mitran <cristimitran@gmail.com>
2018-05-11 17:59:30 +0300

    more work on get all from app

--- qlik-node/playground/fullAppExtract/engine.js
+++ qlik-node/playground/fullAppExtract/engine.js
@@ -3,6 +3,7 @@ const enigma = require('enigma.js')
 const WebSocket = require('ws')
 const bluebird = require('bluebird')
 const schema = require('enigma.js/schemas/12.20.0.json')
+const XLSX = require('xlsx')
 
 
 let session = enigma.create({
@@ -33,7 +34,7 @@ const sheetListRequest = {
 
 async function getStuff() {
     try {
-        let doc = await session.open().then((g) => g.openDoc('Helpdesk-test'))
+        let doc = await session.open().then((g) => g.openDoc('Consumer Sales'))
         let sheetList = await doc.createObject(sheetListRequest).then(object => object.getLayout())
 
         let printSheet = []
@@ -41,7 +42,8 @@ async function getStuff() {
         let objNames = []
         let objLayouts = []
         let objProps = []
-        
+        let result = []
+
 
         sheetList.qAppObjectList.qItems.forEach(function (value) {
             let newTitle = value.qMeta.title
@@ -59,15 +61,58 @@ async function getStuff() {
             let x = await doc.getObject(i).then((o) => o.getLayout([]))
             objLayouts.push(x)
         }
-        
-        console.log(JSON.stringify(printSheet, 0, 2))
-        //console.log(JSON.stringify(objLayouts, 0 , 2))
+
+        // for (let i of objLayouts) {
+        //     if (i.qInfo.qType !== 'map' && i.qInfo.qType !== 'filterpane') {
+        //         for(let j of i.qHyperCube.qDimensionInfo) {
+        //             console.log(j.qFallbackTitle)
+        //         }
+        //     }
+        // }
+
+
+
+        let num = 1
+        for (let i of objLayouts) {
+            for (let j of printSheet) {
+
+                let getDimension = function () {
+                    let str = ""
+                    for (let j of i.qHyperCube.qDimensionInfo) {
+                        str += j.qFallbackTitle + ", "
+                    }
+                    return str.slice(0, -2)
+                }
+
+                let getMeasure = function () {
+                    let str = ""
+                    for (let j of i.qHyperCube.qMeasureInfo) {
+                        str += j.qFallbackTitle + ", "
+                    }
+                    return str.slice(0, -2)
+                }
+
+                if (i.qInfo.qType !== 'map' && i.qInfo.qType !== 'filterpane' && i.qInfo.qId === j.name) {
+                    result.push({
+                        nr: num, sheet: j.sheetTitle, qID: i.qInfo.qId, qType: i.qInfo.qType, dimension: getDimension(), measure: getMeasure(),
+                        title: i.title, subtitle: i.subtitle, footnote: i.footnote
+                    })
+                    num = num + 1
+                }
+            }
+        }
+
+        //console.log(result)
+
+        let ws = XLSX.utils.json_to_sheet(result)
+        let wb = XLSX.utils.book_new()
+        XLSX.utils.book_append_sheet(wb, ws, "People")
+        XLSX.writeFile(wb, "test.xlsx")
+        session.close()
+
+        // console.log(printSheet)
         //console.log(objLayouts)
-        
-        //console.log(objLayouts[1].qHyperCube.qMeasureInfo)
-       
-    
-        //doc.getObject('uETyGUP').then((o) => o.getLayout()).then((x) => console.log(x))
+        //doc.getObject('TAmQxU').then((o) => o.getLayout()).then((x) => console.log(x.qHyperCube.qMeasureInfo))
 
 
 
